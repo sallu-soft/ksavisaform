3032306c2d1fc5cf3c789e9d9c45e89a179db639
Dev-Shakil <mdsalauddinm332@gmail.com>
2025-02-01 13:02:12 +0600

    whatsapp icon

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -897,7 +897,14 @@
 
 
     </div>
-
+    <a
+          href="https://wa.me/+8801609317035"
+          rel="noopener noreferrer"
+          target="_blank"
+          class="fixed bottom-10 right-10 text-6xl text-green-600"
+        >
+        <i class="bi bi-whatsapp"></i>
+</a>
     <div id="preloader"></div>
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
